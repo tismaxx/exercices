29e4d03f72f83a75165a439d699fbd8fa4fcbf2f
unknown <mailys.airouche@mcma-solutions.com>
2022-02-01 15:43:05 +0100

    git(exo4) add exo 4

---
+++ exercice4/index.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: { // You shouldn't change the "data" part
+    array : [7,8,9,5,6,9,8,8,8,8,7,6,9]
+  },
+  methods:{
+  filterDuplicates(data) {
+      // Write your code here
+      // To debug: console.error('Debug messages...');
+  }
+}
+})
